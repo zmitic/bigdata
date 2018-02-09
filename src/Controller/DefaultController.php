889c8363126ef9e8e0fa5ab255a9244fed9c6381
Zeljko Mitic <zeljko.mitic@soprex.com>
2018-02-09 14:00:54 +0100

    First commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="default")
+     */
+    public function index()
+    {
+        // replace this line with your own code!
+        return $this->render('@Maker/demoPage.html.twig', [
+            'path' => str_replace($this->getParameter('kernel.project_dir') . '/', '', __FILE__)
+        ]);
+    }
+}
