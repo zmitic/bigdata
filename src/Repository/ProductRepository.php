a4c6d21a45f94c6ae0758b344c2d16da4496efd0
WebjobsBiz <mitke013@gmail.com>
2018-05-13 00:56:36 +0200

    PHPStan fix

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -5,7 +5,6 @@ namespace App\Repository;
 use App\Entity\Manufacturer;
 use App\Entity\Product;
 use App\Model\BaseRepository;
-use Generator;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class ProductRepository extends BaseRepository
@@ -15,24 +14,26 @@ class ProductRepository extends BaseRepository
         parent::__construct($registry, Product::class);
     }
 
-    public function applyFilters(array $filters): ?Generator
+    public function applyFilters(array $filters): ?iterable
     {
         yield from $this->whereManufacturer($filters['manufacturer'] ?? null);
         yield from $this->whereMinPrice($filters['min_price'] ?? null);
         yield from $this->whereMaxPrice($filters['max_price'] ?? null);
     }
 
-    private function whereMinPrice(?float $minPrice): ?Generator
+    private function whereMinPrice(?float $minPrice): ?iterable
     {
-        yield $this->make($this->expr()->gte('o.basePrice', ':min'), ['min' => $minPrice], $minPrice);
+        return [
+            $this->make($this->expr()->gte('o.basePrice', ':min'), ['min' => $minPrice], $minPrice)
+        ];
     }
 
-    private function whereMaxPrice(?float $maxPrice): ?Generator
+    private function whereMaxPrice(?float $maxPrice): ?iterable
     {
         yield $this->make($this->expr()->lte('o.basePrice', ':max'), ['max' => $maxPrice], $maxPrice);
     }
 
-    private function whereManufacturer(?Manufacturer $manufacturer): ?Generator
+    private function whereManufacturer(?Manufacturer $manufacturer): ?iterable
     {
         if ($manufacturer) {
             yield [$this->expr()->eq('o.manufacturer', ':man'), 'man' => $manufacturer];
